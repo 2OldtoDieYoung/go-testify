eccba1e5ca0b995b53b9172fbb097c3a29c6a4e3
Galkin Denis <mail.galkin.d@yandex.ru>
2024-11-06 13:44:09 +0300

    fix test

--- precode.go
+++ precode.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"net/http"
-	"net/http/httptest"
 	"strconv"
 	"strings"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 var cafeList = map[string][]string{
@@ -46,51 +42,4 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(answer))
-}
-
-// Запрос сформирован корректно, сервис возвращает код ответа 200 и тело ответа не пустое.
-func TestMainHandlerCorrectRequest(t *testing.T) {
-	req, err := http.NewRequest("GET", "/?city=moscow&count=4", nil)
-	assert.NoError(t, err)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	assert.Equal(t, http.StatusOK, responseRecorder.Code)
-
-	assert.NotEmpty(t, responseRecorder.Body.String())
-
-	expected := "Мир кофе, Сладкоежка, Кофе и завтраки, Сытый студент"
-	assert.Equal(t, expected, responseRecorder.Body.String())
-}
-
-// Город, который передаётся в параметре city, не поддерживается. Сервис возвращает код ответа 400 и ошибку wrong city value в теле ответа.
-func TestMainHandlerInvalidCity(t *testing.T) {
-	req, err := http.NewRequest("GET", "/?city=borovichi&count=2", nil)
-	assert.NoError(t, err)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
-
-	expectedError := "wrong city value"
-	assert.Equal(t, expectedError, responseRecorder.Body.String())
-}
-
-// Если в параметре count указано больше, чем есть всего, должны вернуться все доступные кафе.
-func TestMainHandlerCountGreaterThanAvailable(t *testing.T) {
-	req, err := http.NewRequest("GET", "/?city=moscow&count=10", nil)
-	assert.NoError(t, err)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	assert.Equal(t, http.StatusOK, responseRecorder.Code)
-
-	expected := "Мир кофе, Сладкоежка, Кофе и завтраки, Сытый студент"
-	assert.Equal(t, expected, responseRecorder.Body.String())
 }
